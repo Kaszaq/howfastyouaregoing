37da015540d1dea641e63d0d270acd7f49f071fc
Kaszaq <kaszaq@gmail.com>
2017-06-26 23:34:39 +0200

    * further cleanup of CfdData

--- src/main/java/pl/kaszaq/cfd/CfdData.java
+++ src/main/java/pl/kaszaq/cfd/CfdData.java
@@ -1,7 +1,7 @@
 package pl.kaszaq.cfd;
 
 import java.time.LocalDate;
-import java.time.ZonedDateTime;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,35 +13,6 @@ import static pl.kaszaq.utils.DateUtils.printSimpleDate;
 public class CfdData {
 
     SortedMap<LocalDate, DailyStatusChanges> cfdItems = new TreeMap<>();
-
-    private class DailyStatusChanges {
-
-        Map<String, Integer> itemsInStatuses = new HashMap<>();
-
-        public Map<String, Integer> getItemsInStatuses() {
-            return itemsInStatuses;
-        }
-
-        private void transitionedToStatus(String toStatus) {
-            if (toStatus == null) {
-                return;
-            }
-            int toStatusCount = itemsInStatuses.getOrDefault(toStatus, 0) + 1;
-            itemsInStatuses.put(toStatus, toStatusCount);
-        }
-
-        private void transitionedFromStatus(String fromStatus) {
-            if (fromStatus == null) {
-                return;
-            }
-            int fromStatusCount = itemsInStatuses.getOrDefault(fromStatus, 0) - 1;
-            itemsInStatuses.put(fromStatus, fromStatusCount);
-        }
-
-        private int getValueForStatus(String status) {
-            return itemsInStatuses.getOrDefault(status, 0);
-        }
-    }
 
     void addTransition(IssueStatusTransition transition) {
 
@@ -56,19 +27,44 @@ public class CfdData {
         item.transitionedToStatus(toStatus);
         item.transitionedFromStatus(fromStatus);
     }
+    
+    public SortedMap<LocalDate, DailyStatusChanges> getDailyTransitions(){
+        return Collections.unmodifiableSortedMap(cfdItems);
+    }
 
     public void printoutForCfd(List<String> statusOrder) {
         Map<String, Integer> statusValues = new HashMap<>();
         statusOrder.forEach(status -> statusValues.put(status, 0));
         statusOrder.stream().map(o -> "\t" + o).forEach(System.out::print);
         System.out.println("");
-        cfdItems.forEach((k, v) -> {
+        getDailyTransitions().forEach((k, v) -> {
             System.out.print(printSimpleDate(k) + "\t");
             statusOrder.forEach(status -> {
-                Integer newValue = statusValues.merge(status, v.getValueForStatus(status), Integer::sum);
+                Integer newValue = statusValues.merge(status, v.getValueChangeForStatus(status), Integer::sum);
                 System.out.print(newValue + "\t");
             });
             System.out.println("");
         });
+    }
+
+    public static class DailyStatusChanges {
+
+        private final Map<String, Integer> itemsInStatuses = new HashMap<>();
+
+        public Map<String, Integer> getItemsInStatuses() {
+            return itemsInStatuses;
+        }
+
+        private void transitionedToStatus(String toStatus) {
+            itemsInStatuses.merge(toStatus, 1, Integer::sum);
+        }
+
+        private void transitionedFromStatus(String fromStatus) {
+            itemsInStatuses.merge(fromStatus, -1, Integer::sum);
+        }
+
+        public int getValueChangeForStatus(String status) {
+            return itemsInStatuses.getOrDefault(status, 0);
+        }
     }
 }
