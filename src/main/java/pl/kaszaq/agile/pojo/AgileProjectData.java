535bc05b05fa5a457bcddece4a2353282a2ed0b1
Kaszaq <kaszaq@gmail.com>
2017-08-12 01:32:24 +0200

    * lost commit

--- src/main/java/pl/kaszaq/agile/pojo/AgileProjectData.java
+++ src/main/java/pl/kaszaq/agile/pojo/AgileProjectData.java
@@ -1,17 +1,17 @@
 package pl.kaszaq.agile.pojo;
 
 import java.time.ZonedDateTime;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import lombok.Value;
 import pl.kaszaq.agile.IssueData;
 
 @Value
 public class AgileProjectData {
-
     String projectId;
     ZonedDateTime lastUpdatedIssue;
     ZonedDateTime lastUpdated;
     Map<String, IssueData> issues;
+    Set<String> customFieldsNames;
 
 }
