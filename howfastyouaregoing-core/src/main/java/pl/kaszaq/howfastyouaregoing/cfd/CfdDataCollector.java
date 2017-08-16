0ccea29ac33ea2bab74f326fef51f9c0e010b6db
Kaszaq <5243787+Kaszaq@users.noreply.github.com>
2017-08-16 23:31:43 +0200

    * changed groupid and artifact names * included examples

---
+++ howfastyouaregoing-core/src/main/java/pl/kaszaq/howfastyouaregoing/cfd/CfdDataCollector.java
@@ -0,0 +1,41 @@
+package pl.kaszaq.howfastyouaregoing.cfd;
+
+import com.google.common.collect.ImmutableSet;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import pl.kaszaq.howfastyouaregoing.agile.IssueStatusTransition;
+
+public class CfdDataCollector implements Collector<IssueStatusTransition, CfdData, CfdData> {
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return ImmutableSet.of(Characteristics.IDENTITY_FINISH);
+    }
+
+    @Override
+    public Supplier<CfdData> supplier() {
+        return () -> new CfdData();
+    }
+
+    @Override
+    public BiConsumer<CfdData, IssueStatusTransition> accumulator() {
+        return (CfdData t, IssueStatusTransition u) -> t.addTransition(u);
+    }
+
+    @Override
+    public BinaryOperator<CfdData> combiner() {
+        return (CfdData a, CfdData b) -> {
+            throw new UnsupportedOperationException("Not supported yet.");
+        };
+    }
+
+    @Override
+    public Function<CfdData, CfdData> finisher() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
